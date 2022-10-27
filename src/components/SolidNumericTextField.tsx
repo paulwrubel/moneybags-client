4afe7ab3133f2a8faa859e5f74c23354bbe877e9
Paul Wrubel <pwrubel7@gmail.com>
2022-10-26 20:26:32 -0500

    Add ability for numeric box to modify without clearing selection (#27)
    closes #5

--- src/components/SolidNumericTextField.tsx
+++ src/components/SolidNumericTextField.tsx
@@ -24,11 +24,49 @@ function SolidNumericTextField({
         setValueInput(formatCurrencyCents(value, { sign: "" }));
     }, [value]);
 
-    const handleKeypress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
         // console.log(event.key);
         if (event.key === "Enter") {
             (event.target as HTMLElement).blur();
             // console.log("did a blur");
+        }
+    };
+
+    const handleBeforeInput = (
+        event: React.SyntheticEvent<HTMLInputElement> & InputEvent,
+    ) => {
+        // console.log("the event: " + event.data);
+        // console.log("inputType: " + event.inputType);
+        // console.log("+ test: " + /[+\-*/]/.test("+"));
+        // console.log("- test: " + /[+\-*/]/.test("-"));
+        // console.log("* test: " + /[+\-*/]/.test("*"));
+        // console.log("/ test: " + /[+\-*/]/.test("/"));
+        if (/[+\-*/]/.test(event.data ?? "")) {
+            // console.log(event.data);
+            // event.preventDefault();
+            const selection = window.getSelection() ?? document.getSelection();
+            // console.log(selection);
+            if (selection) {
+                // console.log(selection.type);
+                // eslint-disable-next-line no-debugger
+                // debugger;
+                // console.log(
+                //     "has removeAllRanges?: ",
+                //     !!selection.removeAllRanges,
+                // );
+                // console.log("has empty?: ", !!selection.empty);
+                // console.log("has collapeToEnd?: ", !!selection.collapseToEnd);
+                // console.log("1 the selection: " + selection.toString());
+                // selection.selectAllChildren(event.currentTarget);
+                // console.log("2 the selection: " + selection.toString());
+                // event.target?.select();
+                // console.log("3 the selection: " + selection.toString());
+                // selection.removeAllRanges();
+                // selection.empty();
+                // selection.collapseToEnd();
+                event.preventDefault();
+                setValueInput(valueInput + event.data);
+            }
         }
     };
 
@@ -42,8 +80,21 @@ function SolidNumericTextField({
         let valueString = valueInput.replaceAll(",", "");
         valueString = valueString.replaceAll(/[^-()\d+*/.]/g, "");
 
-        value = Function(`"use strict";return (${valueString})`)();
-        if (isNaN(value)) {
+        try {
+            value = Function(`
+            "use strict";
+            return (
+                ${valueString}
+            );
+        `)();
+        } catch (error) {
+            if (error instanceof SyntaxError) {
+                value = 0;
+            } else {
+                console.error(error);
+            }
+        }
+        if (isNaN(value) || !isFinite(value)) {
             value = 0;
         }
         value = Math.round(value * 100);
@@ -61,7 +112,8 @@ function SolidNumericTextField({
     return (
         <SolidTextField
             onChange={handleChange}
-            onKeyDown={handleKeypress}
+            onKeyDown={handleKeyDown}
+            onBeforeInput={handleBeforeInput}
             onFocus={handleFocus}
             onBlur={handleBlur}
             value={valueInput}
